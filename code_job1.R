a6ac64a4ba817e3f90bb4bd50c3b36cd6d73b27b
Amithmg <amithmg6@gmail.com>
2020-10-09 00:04:40 +0530

    sa

--- code_job1.R
+++ code_job1.R
@@ -6,7 +6,7 @@ library(av)
 # library(stringr)
 
 # set the path here
-folder_path = "C:/Users/amith.gurumallapal/vitxt/Intermediate Regression in R/"
+folder_path = "C:/Users/amith.gurumallapal/vitxt/"
 setwd(folder_path)
 # READ THE VIDEO FILE
 video_file=list.files(folder_path,pattern = ".mp4")
