2901756e04bb8636076454d5158a61554fc3de63
Amithmg <amithmg6@gmail.com>
2020-10-09 11:00:14 +0530

    Update code_job1.R

--- code_job1.R
+++ code_job1.R
@@ -6,7 +6,7 @@ library(av)
 # library(stringr)
 
 # set the path here
-folder_path = "C:/Users/amith.gurumallapal/vitxt/Supply Chain Analytics in Python/"
+folder_path = "C:/Users/amith.gurumallapal/vitxt/Streamlined Data Ingestion with pandas/"
 setwd(folder_path)
 # READ THE VIDEO FILE
 video_file=list.files(folder_path,pattern = ".mp4")
