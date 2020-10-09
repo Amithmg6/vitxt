a940b2d1529f62568ad1bd63518185fb176fdcdf
Amithmg <amithmg6@gmail.com>
2020-10-09 16:52:59 +0530

    Update code_off_laptop.R

--- code_off_laptop.R
+++ code_off_laptop.R
@@ -9,77 +9,74 @@ library(av)
 folder_path = "C:/Users/amith.gurumallapal/vitxt/Spatial Analysis with sf and raster in R/"
 setwd(folder_path)
 # READ THE VIDEO FILE
-video_file=list.files(folder_path,pattern = ".mp4")
+video_file=list.files(folder_path,pattern = "video")
 # test1=magick::image_read_video(file.choose())
 vilen=length(video_file)
 
 
+for (i in 1:vilen) {
+  test1=magick::image_read_video(video_file[i])
 
-test1=magick::image_read_video(video_file[1])
+  # pick the right frame
+  test1[1]
+  # test1[20]
+  # test1[2]
+  img_to_text=test1[1]
 
-# pick the right frame
-# test1[1]
-# test1[20]
-# test1[2]
-img_to_text=test1[1]
+  # convert the frame to text
+  img_to_text_complete=magick::image_ocr(img_to_text,language = "eng")
+  img_to_text_complete
 
-# convert the frame to text
-img_to_text_complete=magick::image_ocr(img_to_text,language = "eng")
-img_to_text_complete
+  # clean the text before saving
+  new_video_name=gsub(pattern = "\n",replacement = "",img_to_text_complete)
+  new_video_name=gsub("[[:punct:]]", "",new_video_name)
+  new_video_name
 
-# clean the text before saving 
-new_video_name=gsub(pattern = "\n",replacement = "",img_to_text_complete)
-new_video_name=gsub("[[:punct:]]", "",new_video_name)
-new_video_name
+  video_file
+  ## just renaming the values
+  old_video_name=paste0(folder_path,video_file[i])#"C:/Users/kotre/OneDrive/Documents/vitxt/ARIMA Models in R/",
 
-video_file
-## just renaming the values
-old_video_name=paste0(folder_path,video_file[1])#"C:/Users/kotre/OneDrive/Documents/vitxt/ARIMA Models in R/",
+  # old_video_name=paste0(getwd(),"/",)
+  old_video_name
 
-# old_video_name=paste0(getwd(),"/",)
-old_video_name
-
-final_video_name=paste0(folder_path,new_video_name,".mp4")#"C:/Users/kotre/OneDrive/Documents/vitxt/ARIMA Models in R/",
-
-final_video_name
-
-# copy from old files to new files
-file.rename(from = old_video_name, to = final_video_name)
+  final_video_name=paste0(folder_path,new_video_name,".mp4")#"C:/Users/kotre/OneDrive/Documents/vitxt/ARIMA Models in R/",
 
 
+  final_video_name
 
+  # copy from old files to new files
+  file.rename(from = old_video_name, to = final_video_name)
+
+}
+
+
+# test1=magick::image_read_video(video_file[1])
 # 
-# for (i in 1:vilen) {
-#   test1=magick::image_read_video(video_file[i])
-#   
-#   # pick the right frame
-#   test1[1]
-#   # test1[20]
-#   # test1[2]
-#   img_to_text=test1[1]
-#   
-#   # convert the frame to text
-#   img_to_text_complete=magick::image_ocr(img_to_text,language = "eng")
-#   img_to_text_complete
-#   
-#   # clean the text before saving 
-#   new_video_name=gsub(pattern = "\n",replacement = "",img_to_text_complete)
-#   new_video_name=gsub("[[:punct:]]", "",new_video_name)
-#   new_video_name
-#   
-#   video_file
-#   ## just renaming the values
-#   old_video_name=paste0(folder_path,video_file[i])#"C:/Users/kotre/OneDrive/Documents/vitxt/ARIMA Models in R/",
-#   
-#   # old_video_name=paste0(getwd(),"/",)
-#   old_video_name
-#   
-#   final_video_name=paste0(folder_path,new_video_name,".mp4")#"C:/Users/kotre/OneDrive/Documents/vitxt/ARIMA Models in R/",
-#   
-#   
-#   final_video_name
-#   
-#   # copy from old files to new files
-#   file.rename(from = old_video_name, to = final_video_name)
-#   
-# }
+# # pick the right frame
+# # test1[1]
+# # test1[20]
+# # test1[2]
+# img_to_text=test1[1]
+# 
+# # convert the frame to text
+# img_to_text_complete=magick::image_ocr(img_to_text,language = "eng")
+# img_to_text_complete
+# 
+# # clean the text before saving 
+# new_video_name=gsub(pattern = "\n",replacement = "",img_to_text_complete)
+# new_video_name=gsub("[[:punct:]]", "",new_video_name)
+# new_video_name
+# 
+# video_file
+# ## just renaming the values
+# old_video_name=paste0(folder_path,video_file[1])#"C:/Users/kotre/OneDrive/Documents/vitxt/ARIMA Models in R/",
+# 
+# # old_video_name=paste0(getwd(),"/",)
+# old_video_name
+# 
+# final_video_name=paste0(folder_path,new_video_name,".mp4")#"C:/Users/kotre/OneDrive/Documents/vitxt/ARIMA Models in R/",
+# 
+# final_video_name
+# 
+# # copy from old files to new files
+# file.rename(from = old_video_name, to = final_video_name)
