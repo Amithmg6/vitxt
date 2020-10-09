e65b90e9c2748cd10f0ea807aa7c440fa1ace52a
Amithmg <amithmg6@gmail.com>
2020-10-10 00:51:37 +0530

    cx

--- code_off_laptop.R
+++ code_off_laptop.R
@@ -6,7 +6,7 @@ library(av)
 # library(stringr)
 
 # set the path here
-folder_path = "C:/Users/amith.gurumallapal/vitxt/Introduction to Data Engineering//"
+folder_path = "C:/Users/amith.gurumallapal/vitxt/Introduction to Data Visualization in Python/"
 setwd(folder_path)
 # READ THE VIDEO FILE
 video_file=list.files(folder_path,pattern = ".mp4")
