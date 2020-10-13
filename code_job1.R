6eb2d31873bd06940dec042850ad504c942bdc56
Amithmg <amithmg6@gmail.com>
2020-10-13 13:26:21 +0530

    ds

--- code_job1.R
+++ code_job1.R
@@ -6,7 +6,7 @@ library(av)
 # library(stringr)
 
 # set the path here
-folder_path = ""
+folder_path = "/temp_for_conversion/"
 setwd(folder_path)
 # READ THE VIDEO FILE
 video_file=list.files(folder_path,pattern = ".mp4")
